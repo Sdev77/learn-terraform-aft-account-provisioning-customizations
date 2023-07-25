c74071baf14bd3c87aa742ad913742e1c5365407
Sally <sallydaveytv@gmail.comgit config --global user.name Sdev77>
2023-07-25 22:40:45 +0100

    account customizations

--- terraform/states.tf
+++ terraform/states.tf
@@ -1,5 +1,5 @@
 resource "aws_sfn_state_machine" "aft_account_provisioning_customizations" {
-  name       = "aft-account-provisioning-customizations"
+  name       = "aft-account-provisioning-customisations"
   role_arn   = aws_iam_role.aft_states.arn
   definition = templatefile("${path.module}/states/customizations.asl.json", {})
 }
